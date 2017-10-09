f5b6a7a70b5e240f535fc155e51768a9302820c6
qq57694878 <57694878@qq.com>
2017-10-09 17:30:27 +0800

    搜索

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="root">
-    <div></div>
+
     <div class="panel panel-primary">
       <div class="panel-heading">
         <h3 class="panel-title row"><div class="col-lg-8">{{article.title}}</div> <div class="col-lg-4">id:{{article.id}} - type:{{article.type}} - createtime:{{article.createtime}}</div></h3>
@@ -9,6 +9,7 @@
         <p v-html="article.content"></p>
       </div>
     </div>
+    <div class="goback"><el-button type="primary" @click="goBack">返回</el-button></div>
   </div>
 </template>
 <script>
@@ -32,6 +33,9 @@
         }, (response) => {
           console.log(response);
         });
+      },
+      goBack(){
+        this.$router.go(-1);
       }
 
     },
@@ -58,6 +62,10 @@
     width: 100%;
     padding: 30px 30px;
     text-align: left;
+  }
+  .goback{
+    text-align: right;
+    margin:10px 10px;
   }
 
 </style>
